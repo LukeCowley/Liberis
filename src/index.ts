999cbb5d058a7cad2a5268954bdb7bb2a706b2d7
Luke <lukecowley1987@gmail.com>
2018-07-09 18:15:15 +0100

    commented out in index.js

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { BcaCalculator } from "./bca-calculator";
-import { Application } from './models/application';
+// import { BcaCalculator } from "./bca-calculator";
+// import { Application } from './models/application';
 
-function showHello(divName: string, name: string) {
-    const elt = document.getElementById(divName);
+// function showHello(divName: string, name: string) {
+//     const elt = document.getElementById(divName);
     
-    let timeSpan: YmTimeSpan = {years: "2", months: "3" }
-    let transaction: Transaction = {date: new Date("2018-03-01T00:00:00.000Z"), value: 6966}
-    let transactions = [transaction];
-    let application: Application = {
-        amountRequested: 2000,
-        timeInBusiness: timeSpan,
-        transactions: transactions
-    };
+//     let timeSpan: YmTimeSpan = {years: "2", months: "3" }
+//     let transaction: Transaction = {date: new Date("2018-03-01T00:00:00.000Z"), value: 6966}
+//     let transactions = [transaction];
+//     let application: Application = {
+//         amountRequested: 2000,
+//         timeInBusiness: timeSpan,
+//         transactions: transactions
+//     };
     
-    elt.innerText = BcaCalculator.IsEligible(application).toString();
-}
+//     elt.innerText = BcaCalculator.IsEligible(application).toString();
+// }
 
- showHello("greeting", "TypeScript");
+//  showHello("greeting", "TypeScript");
