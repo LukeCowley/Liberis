850fc29b13257931ae5bc7ff86a00b51eed3db52
Luke <lukecowley1987@gmail.com>
2018-07-09 16:13:41 +0100

    mocha and chai

---
+++ src/bca-calculator.ts
@@ -0,0 +1,16 @@
+export class BcaCalculator{
+    static IsEligible(application: File|Application){
+        return false;
+    } 
+
+    static GetApplicationFromFile(file: File){
+
+    }
+}
+
+function parseFile(file: File){
+    
+    //placeholder below
+    let timeSpan: YmTimeSpan = {years: "2", months: "3" }
+    return timeSpan;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import { sayHello } from "./greet";
+import { BcaCalculator } from "./bca-calculator";
 
 function showHello(divName: string, name: string) {
     const elt = document.getElementById(divName);
-    elt.innerText = sayHello(name);
+    
+    let timeSpan: YmTimeSpan = {years: "2", months: "3" }
+    let transaction: Transaction = {date: new Date("2018-03-01T00:00:00.000Z"), value: 6966}
+    let transactions = [transaction];
+    let application: Application = {
+        amountRequested: 2000,
+        timeInBusiness: timeSpan,
+        transactions: transactions
+    };
+    
+    elt.innerText = BcaCalculator.IsEligible(application).toString();
 }
 
-showHello("greeting", "TypeScript");
+ showHello("greeting", "TypeScript");
