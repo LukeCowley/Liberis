7ccd16c8f86f4c4dd6eb56ca88c90020d6d733e6
Luke <lukecowley1987@gmail.com>
2018-07-09 12:35:33 +0100

    babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const babel = require('gulp-babel');
 
 gulp.task('build', () => {
   return gulp.src('src/**/*.ts')
     .pipe(ts({
         noImplicitAny: true,
-        outFile: "bca-eligibility.js"
+        outFile: 'bca-eligibility.js'
+    }))
+    .pipe(babel({
+        presets: ['env']
     }))
     .pipe(gulp.dest('dist'));
 });
